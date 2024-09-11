719f108e528f7e47eeaa23ae50eecc9b6d90b017
Umar Farouq <umarfarouq447@gmail.com>
2024-09-11 09:56:00 +0100

    Final Chnages

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function lifeInWeeks(age) {
     
-    const yearsRemaining = lifeExpectancy - age;
+    const yearsRemaining = 90 - age;
     const daysRemaining = yearsRemaining * 365;
     const weeksRemaining = yearsRemaining * 52;
     const monthsRemaining = yearsRemaining * 12;
 
-    console.log("You have" + daysRemaining + "days, " + weeksRemaining + "weeks, and" + monthsRemaining + "months left.");
+    console.log("You have " + daysRemaining + " days, " + weeksRemaining + " weeks, and " + monthsRemaining + " months left.");
 }
 
-lifeInWeeks(age);
+lifeInWeeks(20);
