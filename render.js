790fe9ba5967a17b6d95d3333b6d358cd3ac7163
Umar Farouq <umarfarouq447@gmail.com>
2024-09-26 22:13:46 +0100

    render 1s

--- render.js
+++ render.js
@@ -1,1 +1,17 @@
 const ul = document.querySelector(".dc-overview");
+
+const existingListItem = document.querySelectorAll(".dc-overview > li")[1];
+
+const num = 56;
+
+// innerHTML
+// Adding to the ul and adding it at the end
+ul.innerHTML += `<li><strong>You have visited ${num} times</strong></li>`;
+
+// innerAdjacentHTML
+// Create a new <li> element
+const newListItem = document.createElement('li');
+newListItem.textContent = 'I am new'; // Set the text for the new element
+
+// Insert the new <li> at the end of the ul
+ul.insertAdjacentElement("beforeend", newListItem);
